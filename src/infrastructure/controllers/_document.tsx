33625afeab1fe3f72716ad09b8ca535384fa7386
Takashi Machinaga <zxcvbnmtacashi@gmail.com>
2021-12-04 01:44:21 +0900

    chore(analytics): delete analytics code

--- src/infrastructure/controllers/_document.tsx
+++ src/infrastructure/controllers/_document.tsx
@@ -1,11 +1,6 @@
 /* eslint-disable @next/next/no-document-import-in-page */
 import { Head, Html, Main, NextScript } from 'next/document';
 
-const CF_BEACON = {
-  token: '1da2080af4aa4458aa31a10b6bcb56f2',
-  spa: true,
-};
-
 export const MyDocument = () => (
   <Html lang="en-US">
     <Head>
@@ -21,13 +16,6 @@ export const MyDocument = () => (
     <body>
       <Main />
       <NextScript />
-      {/* Cloudflare Web Analytics */}
-      <script
-        defer
-        src="https://static.cloudflareinsights.com/beacon.min.js"
-        data-cf-beacon={JSON.stringify(CF_BEACON)}
-      />
-      {/* End Cloudflare Web Analytics */}
     </body>
   </Html>
 );
