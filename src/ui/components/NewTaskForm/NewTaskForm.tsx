e978d3c6f2b48d9107ecf48a01c60d7b06f08903
Takashi Machinaga <zxcvbnmtacashi@gmail.com>
2022-05-08 03:21:37 +0900

    refactor: use react-hook-form

--- src/ui/components/NewTaskForm/NewTaskForm.tsx
+++ src/ui/components/NewTaskForm/NewTaskForm.tsx
@@ -1,52 +1,46 @@
-import type { ChangeEvent, FormEvent } from 'react';
-import { useCallback, useState } from 'react';
+import { ErrorMessage } from '@hookform/error-message';
+import { useForm } from 'react-hook-form';
 
 import { useNewTaskMutation } from '../../../usecase/useNewTaskMutation';
 import * as classes from './NewTaskForm.css';
 
 export const NewTaskForm: React.FC = () => {
-  const [value, setValue] = useState('');
-
-  const handleChangeText = useCallback((e: ChangeEvent<HTMLInputElement>) => {
-    setValue(e.target.value);
-  }, []);
-
-  const resetValue = useCallback(() => {
-    setValue('');
-  }, []);
-
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm({
+    defaultValues: { title: '' },
+  });
   const { mutate } = useNewTaskMutation();
-  const handleSubmit = useCallback(
-    (e: FormEvent<HTMLFormElement>) => {
-      e.preventDefault();
-
-      if (!value) {
-        return;
-      }
-
-      mutate({ title: value });
-      resetValue();
-    },
-    [mutate, resetValue, value],
-  );
 
   return (
-    <form className={classes.form} onSubmit={handleSubmit}>
-      <input
-        className={classes.input}
-        type="text"
-        name="task"
-        value={value}
-        required
-        maxLength={2 ** 16}
-        autoFocus
-        onChange={handleChangeText}
-        placeholder="Add task"
-        aria-label="New task"
-      />
-      <button className={classes.button} type="submit">
-        Add
-      </button>
-    </form>
+    <>
+      <form
+        className={classes.form}
+        onSubmit={handleSubmit(({ title }) => {
+          mutate({ title });
+          reset();
+        })}
+      >
+        <input
+          {...register('title', { required: true, maxLength: 10 })}
+          className={classes.input}
+          type="text"
+          autoFocus
+          placeholder="Add task"
+          aria-label="New task"
+        />
+        <button
+          className={classes.button}
+          type="submit"
+          disabled={Object.keys(errors).length > 0}
+        >
+          Add
+        </button>
+      </form>
+      <ErrorMessage errors={errors} name="title" />
+    </>
   );
 };
