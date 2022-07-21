9658e3a5353476cffb10326f487cd15d5eb31ae2
Takashi Machinaga <zxcvbnmtacashi@gmail.com>
2022-07-21 23:54:58 +0900

    chore(deps): replace react-query with @tanstack/react-query

--- src/store/ReactQueryClientProvider.tsx
+++ src/store/ReactQueryClientProvider.tsx
@@ -1,7 +1,7 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import type { FC, PropsWithChildren } from 'react';
 import { useMemo } from 'react';
-import { QueryClient, QueryClientProvider } from 'react-query';
-import { ReactQueryDevtools } from 'react-query/devtools';
 
 export const ReactQueryClientProvider: FC<PropsWithChildren<{}>> = ({
   children,
